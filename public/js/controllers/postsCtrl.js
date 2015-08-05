d64341de36f3b63467ae8fcdb143ea8dd4f274c9
reginein <reginein@github.com>
2015-08-05 18:10:36 +0800

    Removed test codes.

--- public/js/controllers/postsCtrl.js
+++ public/js/controllers/postsCtrl.js
@@ -1,9 +1,5 @@
 angular.module('PostsController', ['PostsFactory']).controller('PostsController', function ($scope, PostsFactory) {
-    $scope.regine = "hello";
-    $scope.click = function () {
-        alert("hello kenneth ;)");
-    }
-      PostsFactory.getPosts().then(function (data) {
-         $scope.testfactory = data;
+    PostsFactory.getPosts().then(function (data) {
+        $scope.testfactory = data;
     });
 });
